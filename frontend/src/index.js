936deef5c36299ebe62fbeeffdcfb430e624cdd2
NickSpangler <Spangleyman@mac.com>
2020-11-16 13:03:19 -0500

    add init() function to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,12 @@
 function init() {
     const memeContainer = document.querySelector('#View')
 
+    function getMemes() {
+        fetch(`http://localhost:3000/memes`)
+        .then(resp => resp.json())
+        .then(json => console.log(json))
+    }
+    getMemes()
 }
 
 init()
