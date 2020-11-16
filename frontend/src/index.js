1a6a8c9e8c493c55bfefe843d075f49e6b736fef
NickSpangler <Spangleyman@mac.com>
2020-11-16 17:20:00 -0500

    add remove image function from create panel

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,6 +15,7 @@ function init() {
     getMemes()
 
     const ironmanTab = document.querySelector('#IronmanTab')
+    const createMeme = document.querySelector('#createMeme')
     const createPanels = document.querySelectorAll('.image-select-container')
 
     ironmanTab.onclick = function(e) {
@@ -23,12 +24,21 @@ function init() {
         for (let i=0; i < createPanels.length; i++) {
           if (createPanels[i].classList.contains('empty')) {
             const panel = createPanels[i]
-            panel.querySelector('p').outerHTML = `<img src='${filePath}'><p></p>`
+            panel.querySelector('p').outerHTML = `<img src='${filePath}'>`
             panel.classList.remove('empty');
             break;
           }
         }
         console.log(e.target.attributes.src.value)
+      }
+    }
+
+    createPanel.onclick = function(e) {
+      const regex = RegExp('img');
+      if (regex.test(e.target.outerHTML)) {
+        debugger
+        e.target.parentElement.classList.add('empty');
+        e.target.outerHTML = `<p class='createtext'>Select An Image</p>`
       }
     }
 
