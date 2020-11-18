a24001e9c9f548b821fcbfa0a5ca1f8c33f4fd83
NickSpangler <Spangleyman@mac.com>
2020-11-18 12:41:25 -0500

    link clickevents.js to index.html

--- frontend/src/tabs.js
+++ frontend/src/tabs.js
@@ -1,7 +1,6 @@
 // <------------ CREATE / VIEW TABS ------------->
 
 function openPage(pageName, elmnt) {
-    // Hide all elements with class="tabcontent" by default */
     var i, tabcontent, tablinks;
     tablinks = document.getElementsByClassName('tablink')
     for (let i = 0; i < tablinks.length; i++) {
@@ -12,19 +11,15 @@ function openPage(pageName, elmnt) {
       tabcontent[i].style.display = "none";
     }
   
-    // Show the specific tab content
     elmnt.classList.add('mainactive')
     document.getElementById(pageName).style.display = "block";
-  
   }
   
-  // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
 
   // <------------ PICTURE SELECT TABS ------------->
 
   function openPicPage(pageName, elmnt) {
-    // Hide all elements with class="tabcontent" by default */
     elmnt.classList.add('active')
     var i, pictabcontent, tablinks;
     tablinks = document.getElementsByClassName("pictablink");
@@ -36,11 +31,8 @@ function openPage(pageName, elmnt) {
       pictabcontent[i].style.display = "none";
     }
   
-    // Show the specific tab content
     elmnt.classList.add('active')
     document.getElementById(pageName).style.display = "block";
-  
   }
   
-  // Get the element with id="defaultOpen" and click on it
   document.getElementById("picDefaultOpen").click();
